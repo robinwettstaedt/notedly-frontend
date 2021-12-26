61afa3e049b47db3d26bdca4349c408b6e73ed73
robinwettstaedt <robinwettstadt@gmail.com>
2021-12-26 17:13:04 +0100

    solved svg issues, additional path depth in toolbar defaults, for whatever reason

--- pages/note/[pid].tsx
+++ pages/note/[pid].tsx
@@ -1,12 +1,16 @@
+import { useRouter } from 'next/router';
 import TextEditorDark from '../../components/TextEditor/TextEditorDark';
 import usePrivateRoute from '../../lib/hooks/usePrivateRoute';
 
 const Note = () => {
   usePrivateRoute();
 
+  const router = useRouter();
+  const { pid } = router.query;
+
   return (
     <div>
-      <TextEditorDark noteID="hi" />
+      <TextEditorDark noteID={pid} />
     </div>
   );
 };
